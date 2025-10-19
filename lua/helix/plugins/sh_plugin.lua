0a1b31703c3f6f144731b99f49dd695e1dced6bb
sunmoonyan <pasquier.sosten@gmail.com>
2025-10-19 11:21:59 +0200

    fix bugs

--- lua/helix/plugins/sh_plugin.lua
+++ lua/helix/plugins/sh_plugin.lua
@@ -34,11 +34,70 @@ if SERVER then
         end
     end
 
+function GetCharacterByID(id)
+    for client, character in ix.util.GetCharacters() do
+       if character:GetID() == id then return character end
+    end
+end
 
+function character:Transfer(faction)       
+    if self:GetPlayer():HasWhitelist(ix.faction.GetIndex(faction)) then
+        self:SetFaction(ix.faction.GetIndex(faction))
+    end
+end
 
+net.Receive("ixStorageMoneyTake", function(length, client)
 
+        if (CurTime() < (client.ixStorageMoneyTimer or 0)) then
+            return
+        end
 
+        local character = client:GetCharacter()
 
+        if (!character) then
+            return
+        end
+
+        local storageID = net.ReadUInt(32)
+        local amount = net.ReadUInt(32)
+
+        local inventory = client.ixOpenStorage
+
+        if (!inventory or !inventory.storageInfo or storageID != inventory:GetID()) then
+            return
+        end
+
+        local entity = inventory.storageInfo.entity
+
+        if (!IsValid(entity) or
+            (!entity:IsPlayer() and (!isfunction(entity.GetMoney) or !isfunction(entity.SetMoney))) or
+            (entity:IsPlayer() and !entity:GetCharacter())) then
+            return
+        end
+
+        entity = entity:IsPlayer() and entity:GetCharacter() or entity
+        amount = math.Clamp(math.Round(tonumber(amount) or 0), 0, entity:GetMoney())
+
+        if (amount == 0) then
+            return
+        end
+       
+        if hook.Run("CanTakeMoney",character,entity) == false then return end
+
+        character:SetMoney(character:GetMoney() + amount)
+
+        local total = entity:GetMoney() - amount
+        entity:SetMoney(total)
+
+        net.Start("ixStorageMoneyUpdate")
+            net.WriteUInt(storageID, 32)
+            net.WriteUInt(total, 32)
+        net.Send(inventory:GetReceivers())
+
+        ix.log.Add(client, "storageMoneyTake", entity, amount, total)
+
+        client.ixStorageMoneyTimer = CurTime() + 0.5
+    end)
 
 else
 
